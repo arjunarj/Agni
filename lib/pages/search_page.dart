bfa3d7a9720fd96fa9f74bd737356e7bafb52c6a
arjunarj <nagarjunan66@gmail.com>
2022-08-14 16:11:59 +0530

    Home & Search changes

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -11,25 +11,43 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.all(8),
-          color: Colors.grey[100],
-          child: TextField(
-            cursorColor: Colors.deepPurple,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Search',
-                fillColor: Colors.grey),
+      appBar: AppBar(
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(8),
+            color: Colors.grey[100],
+            child: TextField(
+              cursorColor: Colors.deepPurple,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search',
+                  fillColor: Colors.grey),
+            ),
           ),
         ),
+        toolbarHeight: 80,
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
-      toolbarHeight: 80,
-      backgroundColor: Colors.white,
-      elevation: 0,
-    ));
+      body: GridView.builder(
+        itemCount: 8,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.25,
+        ),
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(14),
+                child: Container(
+                  color: Color.fromARGB(255, 240, 231, 255),
+                ),
+              ));
+        },
+      ),
+    );
   }
 }
